c541ebb8fdf621013790db4316c4e4df6a50e300
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-10 14:44:36 +0600

    dexd

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -74,7 +74,7 @@ function ProfileMenu() {
     );
   };
 
-  const { logOutUser} = useAuth();
+  const { user, logOutUser} = useAuth();
 
 const handelSignOut = () =>{
   logOutUser()
@@ -88,20 +88,29 @@ const handelSignOut = () =>{
 }
   return (
     <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
-     
       <MenuHandler>
         <Button
           variant="text"
           color="blue-gray"
           className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
         >
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="candice wu"
-            className="border border-blue-500 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
+          {user ? (
+            <Avatar
+              variant="circular"
+              size="sm"
+              alt="candice wu"
+              className="border border-blue-500 p-0.5"
+              src={user.profile}
+            />
+          ) : (
+            <Avatar
+              variant="circular"
+              size="sm"
+              alt="candice wu"
+              className="border border-blue-500 p-0.5"
+              src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png"
+            />
+          )}
           <ChevronDownIcon
             strokeWidth={2.5}
             className={`h-3 w-3 transition-transform ${
@@ -115,7 +124,10 @@ const handelSignOut = () =>{
         {renderProfileMenuItem(profileMenuItems[1])}
         {renderProfileMenuItem(profileMenuItems[2])}
         {renderProfileMenuItem(profileMenuItems[3])}
-        <button onClick={handelSignOut} className="w-full">
+        <button
+          onClick={handelSignOut}
+          className="w-full hover:bg-red-500/10 bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
+        >
           {renderProfileMenuItem(profileMenuItems[4])}
         </button>
       </MenuList>
