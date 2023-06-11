bcf04d4bd59275fa4a73b8063c069b78f4d445a5
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-11 11:36:48 +0600

    valu

--- src/Pages/DeshBord/Instructor/AddClass/AddClass.jsx
+++ src/Pages/DeshBord/Instructor/AddClass/AddClass.jsx
@@ -1,35 +1,55 @@
 import { Input } from "@material-tailwind/react";
 import React from "react";
 import useAuth from "../../../../Hooks/useAuth";
+import { useForm } from "react-hook-form";
 
 const AddClass = () => {
   const { user } = useAuth();
+
+   const {
+     register,
+     handleSubmit,
+  
+     formState: { errors },
+   } = useForm();
+   const onSubmit = (data) => console.log(data);
   return (
     <div>
-      <div className="p-14 rounded-lg border-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="p-14 rounded-lg border-2"
+      >
         <div className="flex space-x-5">
           <div className="flex flex-col w-72 gap-6">
-            <Input color="teal" label="Class name" />
-            <Input color="teal" label="Available seats" />
-            <Input color="teal" label="Price" />
+            <Input color="teal" label="Class name" {...register("className")} />
+            <Input
+              color="teal"
+              type="number"
+              label="Available seats"
+              {...register("availableSeats")}
+            />
+            <Input type="number" color="teal" label="Price" {...register("price")} />
           </div>
           <div className="flex flex-col w-72 gap-6">
             <Input
               readOnly
-              defaultValue={user?.displayName}
+              value={user?.displayName}
               color="teal"
               label="Instructor name"
+              {...register("instructorName")}
             />
             <Input
-              defaultValue={user?.email}
+              value={user?.email}
               color="teal"
               label="Instructor email"
               readOnly
+              {...register("instructorEmail")}
             />
             <Input
               type="number"
               color="teal"
-              label="Instructor telephone (optional)"
+              label="Instructor PhoneNumber (optional)"
+              {...register("instructorPhoneNumber")}
             />
           </div>
         </div>
@@ -38,12 +58,17 @@ const AddClass = () => {
             <span className="text-2xl">Pick a Class Image</span>
           </label>
           <input
+            {...register("classImage")}
             type="file"
             className="file-input file-input-bordered w-full max-w-xs"
           />
         </div>
-        <button className="btn  btn-neutral w-full mt-10">Add</button>
-      </div>
+        <input
+          className="btn  btn-neutral w-full mt-10"
+          type="submit"
+          value="Add"
+        />
+      </form>
     </div>
   );
 };
