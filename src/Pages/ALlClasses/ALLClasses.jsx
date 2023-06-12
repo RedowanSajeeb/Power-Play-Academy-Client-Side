75d4615212e0b0fc0998afbdcf670d2bcbf17a44
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-06-12 07:24:31 +0600

    green

--- src/Pages/ALlClasses/ALLClasses.jsx
+++ src/Pages/ALlClasses/ALLClasses.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
-
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+  Button,
+} from "@material-tailwind/react";
 const ALLClasses = () => {
   const [axiosSecure] = useAxiosSecure();
 
@@ -17,20 +24,32 @@ const ALLClasses = () => {
   return (
     <div>
       ------classes
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-10 ">
         {classesALL.map((classA) => (
-          <div key={classA._id} className="card h-[500px] glass p-5">
-            <figure>
-              <img className=" w-full" src={classA.classImage} alt="car!" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">Life hack</h2>
-              <p>How to park your car at your garage?</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Learn now!</button>
-              </div>
-            </div>
-          </div>
+          <Card key={classA._id} className="mt-6 w-96 "> 
+          {/* glass //TODO */}
+            <CardHeader color="blue-gray" className="relative h-56">
+              <img
+                className="w-full h-full"
+                src={classA.classImage}
+                alt="img-blur-shadow"
+                layout="fill"
+              />
+            </CardHeader>
+            <CardBody>
+              <Typography variant="h5" color="blue-gray" className="mb-2">
+                {classA.className}
+              </Typography>
+              <Typography variant="h6" color="blue-gray" className="mb-2">
+                Instructor: {classA.instructorName}
+              </Typography>
+              <h4>Available seats: {classA.availableSeats}</h4>
+              <h4>price: $ {classA.price}</h4>
+            </CardBody>
+            <CardFooter className="pt-0">
+              <Button className="w-full bg-green-700">Select</Button>
+            </CardFooter>
+          </Card>
         ))}
       </div>
     </div>
